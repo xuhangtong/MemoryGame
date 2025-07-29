18fc2af458bab201d0de2257d0c0265890ab8a51
Xuhang <xuhang.tong@gmail.com>
2025-07-28 21:49:15 -0600

    CardView isFaceUp

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -8,15 +8,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View { //this struct behaves like a View
+    //still a variable
+    var body: some View {//some view, the type of variable can be any struct, as long as the struct return a some view behaves like view
+        HStack{
+            CardView(isFaceUp: false)
+            CardView()
+            CardView()
+            CardView()
         }
-        .padding()
+        .foregroundStyle(.green)
+        .imageScale(.small)
+        .padding()//a function, view modifier
+    }//computed properties, code run over and over
+}
+
+struct CardView:View{
+    var isFaceUp:Bool = true //every var
+    
+    var body:some View {
+        ZStack(content: { //embedded function
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 50)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 50)
+                    .strokeBorder(lineWidth: 5)
+                Text("⚽️").font(.largeTitle)
+            }else {
+                RoundedRectangle(cornerRadius: 50)
+            }
+        })
     }
 }
 
