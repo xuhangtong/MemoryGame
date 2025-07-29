a73b77aff768f637a481a962f43153f13a465e14
Xuhang <xuhang.tong@gmail.com>
2025-07-28 22:46:16 -0600

    Togglable isFaceUp

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -12,32 +12,33 @@ struct ContentView: View { //this struct behaves like a View
     //still a variable
     var body: some View {//some view, the type of variable can be any struct, as long as the struct return a some view behaves like view
         HStack{
-            CardView(isFaceUp: false)
-            CardView()
-            CardView()
-            CardView()
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: true)
         }
         .foregroundStyle(.green)
-        .imageScale(.small)
         .padding()//a function, view modifier
     }//computed properties, code run over and over
 }
 
 struct CardView:View{
-    var isFaceUp:Bool = true //every var
+    @State var isFaceUp = true //every var
     
     var body:some View {
-        ZStack(content: { //embedded function
+        ZStack { //embedded function
+            let base = RoundedRectangle(cornerRadius: 50)
             if isFaceUp{
-                RoundedRectangle(cornerRadius: 50)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 50)
-                    .strokeBorder(lineWidth: 5)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 5)
                 Text("⚽️").font(.largeTitle)
             }else {
-                RoundedRectangle(cornerRadius: 50)
+                base.fill()
             }
-        })
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
